6ee7284f5192d17fccb36060d5c697fb05289f07
samikayaa <samikaya89@gmail.com>
2025-05-11 18:22:46 +0300

    c

--- src/components/Gonderiler/Gonderi.jsx
+++ src/components/Gonderiler/Gonderi.jsx
@@ -3,6 +3,51 @@ import Yorumlar from '../Yorumlar/Yorumlar';
 import BegenBolumu from './BegenBolumu';
 import GonderiBasligi from './GonderiBasligi';
 
+
+const Gonderi = (props) => {
+ const { gonderi, gonderiyiBegen } = props;
+ /* ADIM 3: Gönderiyi beğen fonksiyonu bu component'e geliyor ama beğeni sayısı hem yanlış
+            (sahtaVeri'den bakılabilir) hem de artmıyor. problemler neler olabilir? 
+            İpucu: BegenBolumu component'ine ve gonderiyiBegen fonksiyona bakabilirsin
+  */
+ return (
+   <div className="post-border">
+     <GonderiBasligi
+       username={gonderi.username}
+       thumbnailUrl={gonderi.thumbnailUrl}
+     />
+     <div className="post-image-wrapper">
+       <img
+         alt="post thumbnail"
+         className="post-image"
+         src={gonderi.imageUrl}
+       />
+     </div>
+     <BegenBolumu begeniSayisi={gonderi.likes} />
+     <Yorumlar yorumlar={gonderi.comments} />
+   </div>
+ );
+};
+
+
+export default Gonderi;
+
+
+
+
+
+
+
+
+
+
+
+/*
+import React from 'react';
+import Yorumlar from '../Yorumlar/Yorumlar';
+import BegenBolumu from './BegenBolumu';
+import GonderiBasligi from './GonderiBasligi';
+
 const Gonderi = (props) => {
   const { gonderi, gonderiyiBegen } = props;
 
@@ -29,3 +74,5 @@ const Gonderi = (props) => {
 };
 
 export default Gonderi;
+
+*/
--- src/components/Gonderiler/Gonderiler.jsx
+++ src/components/Gonderiler/Gonderiler.jsx
@@ -2,6 +2,48 @@ import React from 'react';
 import Gonderi from './Gonderi';
 import './Gonderiler.css';
 
+
+const Gonderiler = (props) => {
+ /* Adım 1: Bu component kullanıldığı yere bakarak hangi propları almalıyız. */
+
+ const {gonderiyiBegen, gonderiler} = props
+
+ return (
+   <div className="posts-container-wrapper">
+     {/* Adım 2:  Burada bir sorun var gibi. gönderiler için burada bir map yapalım. 
+                  hangi propları göndermeliyiz */
+     
+       gonderiler.map( (post, index) => {
+        return <Gonderi gönderi={post} key={index} gonderiyiBegen={gonderiyiBegen}/>
+       })
+                  
+     }
+   </div>
+ );
+};
+
+
+export default Gonderiler;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+import React from 'react';
+import Gonderi from './Gonderi';
+import './Gonderiler.css';
+
 const Gonderiler = (props) => {
   const { gonderiyiBegen, gonderiler } = props;
 
@@ -17,3 +59,5 @@ const Gonderiler = (props) => {
 };
 
 export default Gonderiler;
+
+*/
